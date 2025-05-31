008bf474eac753201e86271711a4975266c4330b
Adrien B <adrien.bufort@orange.com>
2025-05-31 15:36:19 +0000

    feat: adding (and debugging) dataset

---
+++ meteolibre_model/datasets/dataset_meteofrance.py
@@ -0,0 +1,90 @@
+"""
+This is the module to handle the dataloading from the huggingface dataset
+"""
+
+from datetime import datetime
+import pandas as pd
+import os
+
+import torch
+from datasets import load_dataset
+import numpy as np
+from numpy.random import default_rng
+
+
+def read_record(record):
+    """Reads a single line from the dataset."""
+    radar_back_file = record["radar_file_path_back"]
+    radar_back_future = record["radar_file_path_future"]
+
+    gs_future = record["groundstation_file_path_future"]
+    gs_back = record["groundstation_file_path_back"]
+
+    height = record["ground_height_file_path"]
+
+    hour = record["hour"]
+    minutes = record["minute"]
+    time_radar_back = record["time_radar_back"]
+    datetime = record["datetime"]
+    id = record["id"]
+
+    # Load the all the npz files
+    npz_paths = [radar_back_file, radar_back_future, gs_future, gs_back, height]
+    # Check if all files exist
+    for npz_path in npz_paths:
+        if not os.path.exists(npz_path):
+            print(f"File not found: {npz_path}")
+            continue
+    # Load the npz files
+    try:
+        radar_back_data = np.load(radar_back_file)[
+            "arr_0"
+        ]  # Assuming the data is stored under 'arr_0'
+        radar_future_data = np.load(radar_back_future)["arr_0"]
+        gs_future_data = np.load(gs_future)["arr_0"]
+        gs_back_data = np.load(gs_back)["arr_0"]
+        height_data = np.load(height)["arr_0"]
+    except Exception as e:
+        print(
+            f"Error loading data from {radar_back_file}, {radar_back_future}, {gs_future}, {gs_back}, {height}: {e}"
+        )
+        raise("Error")
+
+    return {
+        "radar_back": radar_back_data,  # The NumPy array itself
+        "radar_future": radar_future_data,  # The NumPy array itself
+        "groundstation_future": gs_future_data,  # The NumPy array itself
+        "groundstation_back": gs_back_data,  # The NumPy array itself
+        "ground_height": height_data,  # The NumPy array itself
+        "hour": hour,  # The scalar value
+        "minutes": minutes,  # The scalar value
+        "time_radar_back": time_radar_back,  # The scalar value
+        "datetime": datetime,  # The scalar value
+        "id": id,  # The scalar value
+    }
+
+
+class MeteoLibreDataset(torch.utils.data.dataset.Dataset):
+    def __init__(self, directory):
+        super().__init__()
+
+        self.directory = directory
+
+        # index reader
+        self.json_path = os.path.join(self.directory, "index.json")
+        self.index_data = pd.read_json(self.json_path, orient="columns", lines=True)
+
+        for columns in self.index_data.columns:
+            if "file" in columns:
+                self.index_data[columns] = directory + self.index_data[columns].str[19:]
+
+    def __len__(self):
+        return len(self.index_data)
+
+    def __getitem__(self, item):
+        row = self.index_data.iloc[item]
+
+        # read data
+        data = read_record(row)
+
+        return data
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -13,115 +13,12 @@ import torch
 import matplotlib.pyplot as plt
 
 
-from meteolibre_model.dataset import (
+from meteolibre_model.datasets.dataset_meteofrance import (
     MeteoLibreDataset,
-    transform_groundstation_data_into_image,
 )
 
+def test_dataset():
+    dataset = MeteoLibreDataset(directory="/teamspace/studios/this_studio/data/data/hf_dataset/")
 
-@pytest.fixture
-def dir_index():
-    """Create test fixtures for each test"""
-    # Create a temporary directory for test files
-    return "/home/adrienbufort/meteolibre_dataset/data"
+    assert isinstance(dataset, MeteoLibreDataset)
 
-
-@pytest.fixture
-def index_file(dir_index):
-    """Create test fixtures for each test"""
-    # Create a temporary directory for test files
-    return os.path.join(dir_index, "index.parquet")
-
-
-@pytest.fixture
-def groundstations_info(dir_index):
-    """Create test fixtures for each test"""
-    # Create a temporary directory for test files
-    return os.path.join(dir_index, "groundstations_filter/total_transformed.parquet")
-
-
-@pytest.fixture
-def groundheight_info(dir_index):
-    """Create test fixtures for each test"""
-    # Create a temporary directory for test files
-    return os.path.join(dir_index, "assets/reprojected_gebco_32630_500m_padded.npy")
-
-
-def test_getitem(index_file, dir_index, groundstations_info, groundheight_info):
-    """Test __getitem__ method"""
-    # Setup mock for h5py.File to return a dict-like object with a 'data' key
-
-    dataset = MeteoLibreDataset(
-        index_file=index_file,
-        dir_index=dir_index,
-        groundstations_info=groundstations_info,
-        ground_height_image=groundheight_info,
-        nb_back_steps=2,
-        nb_future_steps=1,
-    )
-
-    # Get an item
-    item = dataset[5000]
-
-    print(item.keys())
-
-    keys_toplot = [
-        "back_0",
-        "back_1",
-        "future_0",
-        # "ground_station_image_previous",
-        # "ground_station_image_next",
-        "mask_previous",
-        "mask_next",
-        "ground_height_image",
-    ]
-
-
-    for key in keys_toplot:
-        plt.figure()
-
-        array = item[key]
-
-        if "mask" not in key:
-            array[array == array.max()] = 0
-            
-            # print number of non zero element
-
-            
-        else:
-            array = array[:,:, 0]
-            
-            array = np.int8(array)
-            
-            # we want to extand the point with 1 value (with convolution)
-            # import numpy as np
-            from scipy.signal import convolve2d
-            array = convolve2d(array, np.ones((3,3)), mode='same')
-            array = convolve2d(array, np.ones((3,3)), mode='same')
-            array = convolve2d(array, np.ones((3,3)), mode='same')
-            array = convolve2d(array, np.ones((3,3)), mode='same')
-            
-            # clip to 1
-            array = np.clip(array, 0, 1)
-
-        plt.imshow(array)
-        plt.title(key)
-        plt.colorbar()
-
-        plt.savefig(f"/home/adrienbufort/meteolibre_model/tests/{key}.png")
-
-    exit()
-
-    # # becnhmark the speed to get the item
-    # start_time = datetime.datetime.now()
-    # for _ in range(10):
-    #     item = dataset[_]
-    # end_time = datetime.datetime.now()
-    # print(f"Time to get 10 items: {end_time - start_time}")
-
-    # Check if the item has the expected keys
-    assert "back_0" in item
-    assert "back_1" in item
-    assert "future_0" in item
-    assert "ground_station_image_previous" in item
-    assert "ground_station_image_next" in item
