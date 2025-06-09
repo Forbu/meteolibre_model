04cedd13617782902b15dc4f9678ff1e5548ba7f
Adrien B <adrien.bufort@orange.com>
2025-06-09 15:48:03 +0000

    dev: complete new vae architecture

---
+++ meteolibre_model/vae/pl_model_meteofrance_dit_vae.py
@@ -0,0 +1,213 @@
+"""
+In this module we will test a new architecture for the VAE.
+IMG -> Encoder (CNN + DiT) -> Latent Space -> Decoder (DiT + CNN) -> IMG
+"""
+
+import os
+import glob
+
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import lightning.pytorch as pl
+
+import einops
+
+from torch.optim import optimizer
+
+import wandb
+from PIL import Image
+
+from heavyball import ForeachSOAP
+
+from diffusers import AutoencoderKL
+from timm.models.vision_transformer import PatchEmbed
+
+from dit_ml.dit import DiT
+
+
+class VAEMeteoLibrePLModelGrid(pl.LightningModule):
+    """
+    PyTorch Lightning module for the MeteoLibre model.
+
+    VAE autoencoder
+    """
+
+    def __init__(
+        self,
+        learning_rate=1e-6,
+        beta=0.000005,
+        test_dataloader=None,
+        dir_save="../",
+    ):
+        """
+        Initialize the MeteoLibrePLModel.
+
+        Args:
+            input_channels_ground (int): Number of input channels for the ground station image.
+            condition_size (int): Size of the conditioning vector.
+            learning_rate (float, optional): Learning rate for the optimizer. Defaults to 1e-3.
+            nb_back (int, optional): Number of past frames to use as input. Defaults to 3.
+            nb_future (int, optional): Number of future frames to predict. Defaults to 1.
+        """
+        super().__init__()
+        self.model = AutoencoderKL(in_channels=5, out_channels=5, latent_channels=64)
+
+        self.patch_embedder = PatchEmbed(
+            img_size=32, patch_size=2, in_chans=64, embed_dim=128
+        )
+
+        self.dit_encoder = DiT(
+            num_patches=32 * 32 // 4,  # if 2d with flatten size
+            hidden_size=128,
+            depth=3,
+            num_heads=8,
+            causal_block=True,
+            causal_block_size=32 * 32 // 4,
+        )
+
+        self.dit_decoder = DiT(
+            num_patches=32 * 32 // 4,  # if 2d with flatten size
+            hidden_size=128,
+            depth=3,
+            num_heads=8,
+        )
+
+        self.learning_rate = learning_rate
+        self.test_dataloader = test_dataloader
+        self.beta = beta
+
+        self.dir_save = dir_save
+
+    def forward(self, x_image, mask_values):
+        """
+        Forward pass through the model.
+
+        Args:
+            x_image (torch.Tensor): Input image tensor of size (b, nb_frame, c, h, w)
+
+        Returns:
+            torch.Tensor: Output tensor from the model.
+        """
+        batch_size, nb_frame, c, h, w = x_image.shape
+
+        # flatten to get (batch_size * nb_frame, c, h, w)
+        x_image = einops.rearrange(x_image, "b t c h w -> (b t) c h w")
+        latents_sample = self.model.encode(x_image).latent_dist.mean
+
+        # 1. patchify
+        latents_sample_patch = self.patch_embedder(
+            latents_sample
+        )  # size is (batch_size * nb_frame, H * W, embed_dim)
+
+        # 2. pass though DiT
+        # resize to (batch_size, H * W * nb_frame, embed_dim)
+        latents_sample_patch = einops.rearrange(
+            latents_sample_patch, "(b t) nb_patch c -> b (t nb_patch) c", t=nb_frame
+        )
+
+        # pass through DiT encoder
+        dit_encoded_latent = self.dit_encoder(latents_sample_patch)
+
+        # pass through DiT decoder
+        dit_decoded_latent = self.dit_decoder(dit_encoded_latent)
+
+        # 3. unpatchify
+
+
+        # decoder todo
+        
+
+        return final_image, (dit_encoded_latent)
+
+    def training_step(self, batch, batch_idx):
+        """
+        Training step for the PyTorch Lightning module.
+
+        """
+        x_image = batch["target_radar_frames"][:, :, :, [0, -1]]
+
+        # rearrange to (batch_size, 2, 256, 256)
+        # and then to (batch_size * 2, 1, 256, 256)
+        x_image = einops.rearrange(x_image, "b h w c -> (b c) h w")
+        x_image = x_image.unsqueeze(1).repeat(1, 3, 1, 1)
+
+        # Forward pass through the model
+        final_image, (latents_mean, latents_variance) = self(x_image)
+
+        reconstruction_loss = F.mse_loss(final_image, x_image)
+
+        self.log("reconstruction_loss", reconstruction_loss)
+
+        kl_loss = torch.mean(
+            -0.5
+            * torch.sum(
+                1 + latents_variance - latents_mean**2 - latents_variance.exp(), dim=1
+            ),
+        )
+
+        self.log("kl_loss", kl_loss)
+
+        loss = reconstruction_loss + self.beta * kl_loss
+
+        return loss
+
+    def configure_optimizers(self):
+        """
+        Configures the optimizer for training.
+
+        Returns:
+            torch.optim.Optimizer: Adam optimizer.
+        """
+        # optimizer = torch.optim.AdamW(self.parameters(), lr=self.learning_rate)
+        optimizer = ForeachSOAP(self.parameters(), lr=self.learning_rate, foreach=False)
+        return optimizer
+
+    @torch.no_grad()
+    def generate_one(self, nb_batch=1, nb_step=100):
+        # Assuming batch is a dictionary returned by TFDataset
+        # generate a random (nb_batch, 1, 256, 256) tensor
+        for batch in self.test_dataloader:
+            break
+
+        x_image = batch["target_radar_frames"][:, :, :, [0]]
+        x_image = x_image.permute(0, 3, 1, 2)
+        x_image = x_image.to(self.device).repeat(1, 3, 1, 1)
+
+        # Forward pass through the model
+        final_image, _ = self(x_image)
+
+        return final_image.permute(0, 2, 3, 1), x_image.permute(0, 2, 3, 1)
+
+    # on epoch end of training
+    def on_train_epoch_end(self):
+        # generate image
+        self.eval()
+
+        result, x_image_future = self.generate_one(nb_batch=1, nb_step=100)
+
+        self.save_image(result[0, :, :, 0].cpu().numpy(), name_append="result")
+        self.save_image(x_image_future[0, :, :, 0].cpu().numpy(), name_append="future")
+
+        self.train()
+
+    def save_image(self, result, name_append="result"):
+        radar_image = result
+
+        fname = (
+            self.dir_save + f"data/{name_append}_radar_epoch_{self.current_epoch}.png"
+        )
+
+        plt.figure(figsize=(20, 20))
+        plt.imshow(radar_image, vmin=-1, vmax=2)
+        plt.colorbar()
+
+        plt.savefig(fname, bbox_inches="tight", pad_inches=0)
+        plt.close()
+
+        # logging image into wandb
+        self.logger.log_image(
+            key=name_append, images=[wandb.Image(fname)], caption=[name_append]
+        )
